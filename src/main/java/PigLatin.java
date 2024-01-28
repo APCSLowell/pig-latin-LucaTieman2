c9724d4a395cccd2457ceb5bd3d059041b103e79
LucaTieman2 <143014925+LucaTieman2@users.noreply.github.com>
2024-01-27 16:26:54 -0800

    Update PigLatin.java

--- src/main/java/PigLatin.java
+++ src/main/java/PigLatin.java
@@ -47,7 +47,7 @@ public class PigLatin {
 		return sWord + "ay";
 	    } else if (findFirstVowel(sWord == 0) {
 		return sWord + "way";
-	    } else if (sWord.substring(0, 2).equals("qu") {
+	    } else if (sWord.substring(0, 2).equals("qu")) {
 	    	return sWord.substring(2, sWord.length()) + sWord.substring(0, 2) + "ay";
     	    } else if (findFirstVowel(sWord) != 0 && findFirstVowel(sWord) != -1) {
 	    	int first = findFirstVowel(sWord);
